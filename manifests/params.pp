e0ee41e96c04c296f3b5e936749a9342f4028f55
Paul Namuag <paultoytoy@gmail.com>
2020-10-19 20:01:36 +0800

    remove definition of apache log dir but add the variable just in case

--- manifests/params.pp
+++ manifests/params.pp
@@ -21,6 +21,18 @@ class clustercontrol::params {
 			$loc_dependencies  = [
 			'httpd', 'wget', 'mailx', 'curl', 'cronie', 'bind-utils', 'php', 'php-gd', 'php-ldap', 'mod_ssl', 'openssl', 'clustercontrol-notifications', 'clustercontrol-ssh', 'clustercontrol-cloud', 'clustercontrol-clud', 's9s-tools'
 			]
+			
+			$apache_log_dir = "/var/log/httpd/"
+			$apache_conf_file = '/etc/httpd/conf/httpd.conf'
+			$apache_ssl_conf_file = '/etc/httpd/conf.d/ssl.conf'
+			$cert_file        = '/etc/pki/tls/certs/s9server.crt'
+			$key_file         = '/etc/pki/tls/private/s9server.key'
+			$apache_user      = 'apache'
+			$apache_service   = 'httpd'
+			$wwwroot          = '/var/www/html'
+			$mysql_cnf        = '/etc/my.cnf'
+			
+			
 				
 			if ($lsbmajdistrelease == 7) {
 				$mysql_packages   = ['mariadb','mariadb-server']
@@ -41,15 +53,6 @@ class clustercontrol::params {
 				
 			}
 			
-			$apache_conf_file = '/etc/httpd/conf/httpd.conf'
-			$apache_ssl_conf_file = '/etc/httpd/conf.d/ssl.conf'
-			$cert_file        = '/etc/pki/tls/certs/s9server.crt'
-			$key_file         = '/etc/pki/tls/private/s9server.key'
-			$apache_user      = 'apache'
-			$apache_service   = 'httpd'
-			$wwwroot          = '/var/www/html'
-			$mysql_cnf        = '/etc/my.cnf'
-
 			notify{"<<<<<<<<<<<<<CC Debugger:>>>>>>>>>>>>>s9s tool reponame: ${$s9s_tools_repo_osname}, \
 				os_majrelease: ${$os_majrelease}, ${ipaddress_lo}, codename: ${lsbdistcodename} , \
 				lsbmajdistrelease: ${lsbmajdistrelease} and data-type is: ${typevar}), \
@@ -99,12 +102,29 @@ class clustercontrol::params {
 				
 				
 				/*notify{"<<<<<<<<<<<<<CC Debugger:>>>>>>>>>>>>>The value is: ${lower_operatingsystem}  and ${ipaddress_lo} and ${lsbdistcodename} and ${lsbmajdistrelease} and data-type is: ${typevar})": }*/
-				
+			
+				$apache_log_dir = "/var/log/apache2/"
 				$wwwroot          = '/var/www/html'
 				$apache_conf_file = '/etc/apache2/sites-available/s9s.conf'
 				$apache_target_file = '/etc/apache2/sites-enabled/001-s9s.conf'
 				$apache_ssl_conf_file = '/etc/apache2/sites-available/s9s-ssl.conf'
 				$apache_ssl_target_file = '/etc/apache2/sites-enabled/001-s9s-ssl.conf'
+				
+
+				$cert_file        = '/etc/ssl/certs/s9server.crt'
+				$key_file         = '/etc/ssl/private/s9server.key'
+				$apache_user      = 'www-data'
+				$apache_service   = 'apache2'
+				$mysql_service    = 'mysql'
+				$mysql_cnf        = '/etc/mysql/my.cnf'
+				$repo_source      = '/etc/apt/sources.list.d/s9s-repo.list'
+				$repo_tools_src   = '/etc/apt/sources.list.d/s9s-tools.list'
+			
+			
+				$mysql_packages   = ['mysql-client','mysql-server']
+				$cc_dependencies  = [
+					'apache2', 'wget', 'mailutils', 'curl', 'dnsutils', 'php-common', 'php-mysql', 'php-gd', 'php-ldap', 'php-curl', 'libapache2-mod-php', 'php-json', 'clustercontrol-notifications', 'clustercontrol-ssh', 'clustercontrol-cloud', 'clustercontrol-clud', 's9s-tools'
+				]
 
 				/* Remove unwanted config files, retain only s9s config files */
 				file {
@@ -123,22 +143,6 @@ class clustercontrol::params {
 				$apache_ssl_target_file = '/etc/apache2/sites-enabled/default-ssl.conf'
 				$apache_httpd_extra_options  = ''
 			}
-
-			$cert_file        = '/etc/ssl/certs/s9server.crt'
-			$key_file         = '/etc/ssl/private/s9server.key'
-			$apache_user      = 'www-data'
-			$apache_service   = 'apache2'
-			$mysql_service    = 'mysql'
-			$mysql_cnf        = '/etc/mysql/my.cnf'
-			$repo_source      = '/etc/apt/sources.list.d/s9s-repo.list'
-			$repo_tools_src   = '/etc/apt/sources.list.d/s9s-tools.list'
-			
-			
-			$mysql_packages   = ['mysql-client','mysql-server']
-			$cc_dependencies  = [
-				'apache2', 'wget', 'mailutils', 'curl', 'dnsutils', 'php-common', 'php-mysql', 'php-gd', 'php-ldap', 'php-curl', 'libapache2-mod-php', 'php-json', 'clustercontrol-notifications', 'clustercontrol-ssh', 'clustercontrol-cloud', 'clustercontrol-clud', 's9s-tools'
-			]
-			
 
 			exec { 'apt-update-severalnines' :
 				path        => ['/bin','/usr/bin'],
