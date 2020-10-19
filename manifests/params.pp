e332bb3ec441c9d3e43bdc05386c55733223d611
Paul Namuag <paultoytoy@gmail.com>
2020-10-19 19:15:13 +0800

    test changes

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,13 +22,18 @@ class clustercontrol::params {
 				os_majrelease: ${$os_majrelease}, ${ipaddress_lo}, codename: ${lsbdistcodename} , \
 				lsbmajdistrelease: ${lsbmajdistrelease} and data-type is: ${typevar})": 
 			}
+			$cc_dependencies  = [
+			'httpd', 'wget', 'mailx', 'curl', 'cronie', 'nmap-ncat', 'bind-utils', 'php', 'php-gd', 'php-ldap', 'mod_ssl', 'openssl', 'clustercontrol-notifications', 'clustercontrol-ssh', 'clustercontrol-cloud', 'clustercontrol-clud', 's9s-tools'
+			]
 				
 			if ($lsbmajdistrelease > 6) {
 				$mysql_packages   = ['mariadb','mariadb-server']
 				$mysql_service    = 'mariadb'
-				$cc_dependencies  = [
-				'httpd', 'wget', 'mailx', 'curl', 'cronie', 'nmap-ncat', 'bind-utils', 'php', 'php-mysql', 'php-gd', 'php-ldap', 'mod_ssl', 'openssl', 'clustercontrol-notifications', 'clustercontrol-ssh', 'clustercontrol-cloud', 'clustercontrol-clud', 's9s-tools'
-				]
+				$cc_dependencies[] = ['php-mysql']
+			} elsif ($lsbmajdistrelease > 7) {
+				$mysql_packages   = ['mariadb','mariadb-server']
+				$mysql_service    = 'mariadb'
+				$cc_dependencies[] = ['php-mysqlnd']
 			} else {
 				$mysql_packages   = ['mysql','mysql-server']
 				$mysql_service    = 'mysqld'
