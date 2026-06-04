c1878dea84d9675292868a9d6eaf16d9b7ef57db
Jessie Baynas <jessie@severalnines.com>
2026-06-04 15:03:37 +0800

    Fix idempotency check for cmon user creation The grant either exists or it doesn't, regardless of networking.

--- manifests/configure_mysql.pp
+++ manifests/configure_mysql.pp
@@ -106,7 +106,7 @@ class clustercontrol::configure_mysql {
       command  => "mysql -u root -p\"${mysql_root_pass}\" -NBe \"CREATE USER IF NOT EXISTS '${cmon_user}'@'${host}' IDENTIFIED BY '${cmon_pass}'; ALTER USER '${cmon_user}'@'${host}' IDENTIFIED BY '${cmon_pass}'; GRANT ALL PRIVILEGES ON *.* TO '${cmon_user}'@'${host}' WITH GRANT OPTION; FLUSH PRIVILEGES;\"",
       path     => ['/bin', '/usr/bin', '/usr/local/bin'],
       provider => shell,
-      unless   => "[ ! -x /usr/bin/mysql ] || mysql -u ${cmon_user} -p\"${cmon_pass}\" -h${host} -NBe 'SELECT 1;' >/dev/null 2>&1",
+      unless   => "[ ! -x /usr/bin/mysql ] || mysql -u root -p\"${mysql_root_pass}\" -NBe \"SELECT 1 FROM mysql.user WHERE User='${cmon_user}' AND Host='${host}';\" 2>/dev/null | grep -q 1",
       require  => [
         Service[$mysql_daemon],
         File['/root/.my.cnf'],
