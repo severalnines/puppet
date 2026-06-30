ace67d102951bac8cce2848fd29987ccee1b55fa
Jessie Baynas <jessie@severalnines.com>
2026-06-30 18:22:39 +0800

    Change package from mariadb-server to mysql-server

--- spec/classes/clustercontrol_spec.rb
+++ spec/classes/clustercontrol_spec.rb
@@ -292,7 +292,7 @@ describe 'clustercontrol' do
     end
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_class('clustercontrol::install::debian') }
-    it { is_expected.to contain_package('mariadb-server').with_ensure('present') }
+    it { is_expected.to contain_package('mysql-server').with_ensure('present') }
   end
 
   # =========================================================================
