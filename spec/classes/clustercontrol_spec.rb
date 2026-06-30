f21e8927662f924ef066314f1a0abeb5ebaf248a
Jessie Baynas <jessie@severalnines.com>
2026-06-30 18:16:34 +0800

    Change service from 'mariadb' to 'mysqld'
    For EL9 the database daemon is mysqld in this PR; asserting mariadb here will fail.

--- spec/classes/clustercontrol_spec.rb
+++ spec/classes/clustercontrol_spec.rb
@@ -173,7 +173,7 @@ describe 'clustercontrol' do
     it { is_expected.to contain_package('s9s-tools') }
 
     # Critical services
-    it { is_expected.to contain_service('mariadb').with_ensure('running') }
+    it { is_expected.to contain_service('mysqld').with_ensure('running') }
     it { is_expected.to contain_service('cmon').with_ensure('running') }
     it { is_expected.to contain_service('cmon-proxy').with_ensure('running') }
 
